b4675964db2212dbfa7073c2fe3cbd077e5314c9
Dmitry Astapov <dastapov@gmail.com>
2018-04-12 16:43:22 +0100

    Fix: parens in record update where defaul val is trivial

--- src/Ast.ml
+++ src/Ast.ml
@@ -937,6 +937,9 @@ end = struct
       | Pexp_record (flds, _)
         when List.exists flds ~f:(fun (_, e0) -> e0 == exp) ->
           exposed Non_apply exp (* Non_apply is perhaps pessimistic *)
+      | Pexp_record (_, Some ({pexp_desc= Pexp_apply ({pexp_desc= Pexp_ident {txt= Lident i}},_) } as e0))
+        when e0 == exp && is_prefix_id i ->
+          false
       | Pexp_record (_, Some ({pexp_desc= Pexp_apply _} as e0))
         when e0 == exp ->
           true
