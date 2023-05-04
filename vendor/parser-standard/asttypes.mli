3666a3dc8b091601703329431fa08ed61ee21b0c
Chris Casinghino <ccasinghino@janestreet.com>
2023-05-04 17:29:22 -0400

    support for layout annotations on type parameters
    Signed-off-by: Chris Casinghino <ccasinghino@janestreet.com>

--- vendor/parser-standard/asttypes.mli
+++ vendor/parser-standard/asttypes.mli
@@ -48,6 +48,15 @@ type global_flag =
   | Global
   | Nothing
 
+(* constant layouts are parsed as layout annotations, and also used
+   in the type checker as already-inferred (i.e. non-variable) layouts *)
+type const_layout =
+  | Any
+  | Value
+  | Void
+  | Immediate64
+  | Immediate
+
 type label = string
 
 type arg_label =
