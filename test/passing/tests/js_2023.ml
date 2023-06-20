b6e5a19f21aef0d2215d201b51d834725901a7f0
Carl Eastlund <ceastlund@janestreet.com>
2023-06-20 17:11:37 -0400

    second line wrapping issue

--- test/passing/tests/js_2023.ml
+++ test/passing/tests/js_2023.ml
@@ -9,3 +9,7 @@
 
 (* xxxxxxxxxxxxxxxxxxxxxxxxxxx [xxxxxxx
    xxxx] xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx [xxxxxxx] *)
+
+(* 0_________10________20________30________40________50________60________70________80
+   This is quite a long line, and ocamlformat doesn't auto-wrap it at the end of the max line length until I put a bracket pair like [this] and now we're on a new line.
+*)
