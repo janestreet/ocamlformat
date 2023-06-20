60841283ed58abd550458ba472361fe45c8b4d63
Carl Eastlund <ceastlund@janestreet.com>
2023-06-20 17:13:32 -0400

    reproduce extra line errors

--- test/passing/tests/js_2023.ml
+++ test/passing/tests/js_2023.ml
@@ -13,3 +13,23 @@
 (* 0_________10________20________30________40________50________60________70________80
    This is quite a long line, and ocamlformat doesn't auto-wrap it at the end of the max line length until I put a bracket pair like [this] and now we're on a new line.
 *)
+
+(** Unexpected blank lines. *)
+
+(*
+   List 1:
+   - hi
+   - bye
+
+   List 2:
+   + hello
+   + goodbye
+*)
+
+(*
+   My code:
+   {[
+     let x = 1
+   ]}
+   See the code?
+*)
