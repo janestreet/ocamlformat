d4608aa3922bcc0751d1b7285b50b32e81cd9b37
Antal Spector-Zabusky <antal.b.sz@gmail.com>
2023-07-31 15:25:07 -0400

    Only remove erasable `Jane_syntax` attributes from the old AST
    Signed-off-by: Antal Spector-Zabusky <antal.b.sz@gmail.com>

--- lib/Normalize_std_ast.ml
+++ lib/Normalize_std_ast.ml
@@ -69,7 +69,7 @@ let docstring (c : Conf.t) =
 let sort_attributes : attributes -> attributes =
   List.sort ~compare:Poly.compare
 
-let make_mapper conf ~ignore_doc_comments =
+let make_mapper conf ~ignore_doc_comments ~erase_jane_syntax =
   let open Ast_helper in
   (* remove locations *)
   let location _ _ = Location.none in
@@ -104,7 +104,7 @@ let make_mapper conf ~ignore_doc_comments =
   (* sort attributes *)
   let attributes (m : Ast_mapper.mapper) (atrs : attribute list) =
     let atrs =
-      if Erase_jane_syntax.should_erase () then
+      if erase_jane_syntax then
         List.filter atrs ~f:(fun a -> not (is_erasable_jane_syntax a))
       else atrs
     in
@@ -168,12 +168,12 @@ let make_mapper conf ~ignore_doc_comments =
   ; pat
   ; typ }
 
-let ast fragment ~ignore_doc_comments c =
-  map fragment (make_mapper c ~ignore_doc_comments)
+let ast fragment ~ignore_doc_comments ~erase_jane_syntax c =
+  map fragment (make_mapper c ~ignore_doc_comments ~erase_jane_syntax)
 
-let equal fragment ~ignore_doc_comments c ast1 ast2 =
+let equal fragment ~ignore_doc_comments ~erase_jane_syntax c ~old:ast1 ~new_:ast2 =
   let map = ast fragment c ~ignore_doc_comments in
-  equal fragment (map ast1) (map ast2)
+  equal fragment (map ~erase_jane_syntax ast1) (map ~erase_jane_syntax:false ast2)
 
 let ast = ast ~ignore_doc_comments:false
 
@@ -204,15 +204,19 @@ let docstrings (type a) (fragment : a t) s =
   let (_ : a) = map fragment (make_docstring_mapper docstrings) s in
   !docstrings
 
-let docstring conf =
-  let mapper = make_mapper conf ~ignore_doc_comments:false in
+let docstring conf ~erase_jane_syntax  =
+  let mapper = make_mapper conf ~ignore_doc_comments:false ~erase_jane_syntax in
   let normalize_code = normalize_code conf mapper in
   docstring conf ~normalize_code
 
-let moved_docstrings fragment c s1 s2 =
+let moved_docstrings fragment ~erase_jane_syntax c ~old:s1 ~new_:s2 =
   let d1 = docstrings fragment s1 in
   let d2 = docstrings fragment s2 in
-  let equal (_, x) (_, y) = String.equal (docstring c x) (docstring c y) in
+  let equal ~old:(_, x) ~new_:(_, y) =
+    String.equal
+      (docstring c x ~erase_jane_syntax)
+      (docstring c y ~erase_jane_syntax:false)
+  in
   let cmt_kind = `Doc_comment in
   let cmt (loc, x) = Cmt.create x loc in
   let dropped x = {Cmt.kind= `Dropped (cmt x); cmt_kind} in
@@ -221,11 +225,19 @@ let moved_docstrings fragment c s1 s2 =
   match List.zip d1 d2 with
   | Unequal_lengths ->
       (* We only return the ones that are not in both lists. *)
-      let l1 = List.filter d1 ~f:(fun x -> not (List.mem ~equal d2 x)) in
+      let l1 =
+        List.filter d1 ~f:(fun old ->
+          List.for_all d2 ~f:(fun new_ ->
+            not (equal ~old ~new_)))
+      in
       let l1 = List.map ~f:dropped l1 in
-      let l2 = List.filter d2 ~f:(fun x -> not (List.mem ~equal d1 x)) in
+      let l2 =
+        List.filter d2 ~f:(fun new_ ->
+          List.for_all d1 ~f:(fun old ->
+            not (equal ~old ~new_)))
+      in
       let l2 = List.map ~f:added l2 in
       List.rev_append l1 l2
   | Ok l ->
-      let l = List.filter l ~f:(fun (x, y) -> not (equal x y)) in
+      let l = List.filter l ~f:(fun (old, new_) -> not (equal ~old ~new_)) in
       List.map ~f:modified l
