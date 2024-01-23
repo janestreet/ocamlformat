7248f2f3afac5a64822e3b72e83cdda2abb21cdd
Chris Casinghino <ccasinghino@janestreet.com>
2024-01-23 18:33:03 +0000

    Fix bug where `42, ~x:(fun x -> x)` would be misformatted
    (The parens would be dropped)

--- test/passing/tests/labeled_tuples_regressions.ml
+++ test/passing/tests/labeled_tuples_regressions.ml
@@ -19,3 +19,54 @@ let foo a =
     , ~q:M.(A)
     , ~r:M.(A 42) ) -> false
 ;;
+
+let bar =
+  ( ~a:foo
+  , ~b:42
+  , ~c:(let x = 18 in
+        x)
+  , ~d:(function
+         | x -> x)
+  , ~e:(fun x -> x)
+  , ~f:(foo 42)
+  , ~g:(match () with
+        | () -> ())
+  , ~h:(try () with
+        | _ -> ())
+  , ~i:(1, 2)
+  , ~j:(~x:1, ~y:2)
+  , ~k:None
+  , ~l:(Some 42)
+  , ~m:`A
+  , ~n:(`B 42)
+  , ~o:{ x = 42; z = false }
+  , ~p:foo.lbl
+  , ~q:((foo 42).lbl)
+  , ~r:(foo.lbl <- 42)
+  , ~s:[| 1; 2 |]
+  , ~t:[: 1; 2 :]
+  , ~u:[ 1; 2 ]
+  , ~v:[ a for a = 1 to 10 ]
+  , ~w:(if true then true else false)
+  , ~x:(();
+        ())
+  , ~y:(while true do
+          ()
+        done)
+  , ~z:(for i = 1 to 2 do
+          ()
+        done)
+  , ~z:(42 : int)
+  , ~y:(42 :> int)
+  , ~x:(42 : int :> bool)
+  , ~w:foo#bar
+  , ~v:foo #~# bar
+  , ~u:(new M.c)
+  , ~t:(x <- 2)
+  , ~s:{<x = 42; y = false>}
+  , ~r:(let module M = N in
+       ())
+  , ~q:(let exception Ex in
+       ())
+  , ~p:(assert true) )
+;;
