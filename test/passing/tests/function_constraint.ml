55be4b11137f08824772e3fb1afbcd1a171138ad
Charlie Gunn <cgunn@janestreet.com>
2024-08-02 16:04:51 -0400

    improve tests
    Signed-off-by: Charlie Gunn <cgunn@janestreet.com>

--- test/passing/tests/function_constraint.ml
+++ test/passing/tests/function_constraint.ml
@@ -1,19 +1,34 @@
-let f = fun (type a) -> (() : unit)
+(* RHS annotated with a type *)
+let f (type a) = (() : unit)
 
-let f = fun (type a : immediate) -> (() : unit)
+let f (type a : immediate) = (() : unit)
 
-let f = fun (type a) x -> (() : unit)
+let f x y = (() : unit)
 
-let f = fun x y -> (() : unit)
+(* RHS annotated with a type and mode *)
+let f (type a) = (() : unit @@ local)
 
-let f = fun (type a : value) x -> (() : unit)
+let f (type a : value) = (() : unit @@ local)
 
-let f = fun (type a) -> (() : unit @@ local)
+let f x y = (() : unit @@ local)
 
-let f = fun (type a : immediate) -> (() : unit @@ local)
+(* LHS and RHS annotated with the same type *)
+let f (type a) : unit = (() : unit)
 
-let f = fun (type a) x -> (() : unit @@ local)
+let f (type a : value) : unit = (() : unit)
 
-let f = fun x y -> (() : unit @@ local)
+let f x y : unit = (() : unit)
 
-let f = fun (type a : value) x -> (() : unit @@ local)
+(* LHS and RHS annotated with different types *)
+let f (type a) : unit = (() : int)
+
+let f (type a : value) : unit = (() : int)
+
+let f x y : unit = (() : int)
+
+(* LHS annotated with a mode, RHS annotated with a type *)
+let (f @ local) (type a) = (() : unit)
+
+let (f @ local) (type a : value) = (() : unit)
+
+let (f @ local) x y  = (() : unit)
