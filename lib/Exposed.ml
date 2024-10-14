10dae1a3c2e468e4e49346d699985430cc4bb4f5
David Vulakh <dvulakh@janestreet.com>
2024-10-14 09:33:28 -0400

    fix angle bracket check bug
    Signed-off-by: David Vulakh <dvulakh@janestreet.com>

--- lib/Exposed.ml
+++ lib/Exposed.ml
@@ -111,9 +111,9 @@ module Right = struct
     | {pval_type= ct; _} -> core_type ct
 
   let rec jkind = function
-  | Default | Abbreviation _ | Mod _ -> false
-  | With (_, t) | Kind_of t -> core_type t
-  | Product jks -> list ~elt:jkind jks
+    | Default | Abbreviation _ | Mod _ -> false
+    | With (_, t) | Kind_of t -> core_type t
+    | Product jks -> list ~elt:jkind (List.map ~f:(fun {txt; _} -> txt) jks)
 
   let structure_item {pstr_desc; pstr_loc= _} =
     match pstr_desc with
@@ -121,12 +121,10 @@ module Right = struct
     | Pstr_typext te -> type_extension te
     | Pstr_exception te -> type_exception te
     | Pstr_primitive vd -> value_description vd
-    | Pstr_kind_abbrev (_, {txt= jk; _}) ->
-      core_type jk
+    | Pstr_kind_abbrev (_, {txt= jk; _}) -> jkind jk
     | Pstr_module _ | Pstr_recmodule _ | Pstr_modtype _ | Pstr_open _
-     |Pstr_kind_abbrev _ | Pstr_class _ | Pstr_class_type _
-     |Pstr_include _ | Pstr_attribute _ | Pstr_extension _ | Pstr_value _
-     |Pstr_eval _ ->
+     |Pstr_class _ | Pstr_class_type _ | Pstr_include _ | Pstr_attribute _
+     |Pstr_extension _ | Pstr_value _ | Pstr_eval _ ->
         false
 
   let signature_item {psig_desc; psig_loc= _} =
@@ -136,12 +134,10 @@ module Right = struct
     | Psig_typesubst typedecls -> list ~elt:type_declaration typedecls
     | Psig_typext te -> type_extension te
     | Psig_exception te -> type_exception te
-    | Psig_kind_abbrev (_, {txt= jk; _}) ->
-      core_type jk
+    | Psig_kind_abbrev (_, {txt= jk; _}) -> jkind jk
     | Psig_module _ | Psig_modsubst _ | Psig_recmodule _ | Psig_modtype _
-     |Psig_kind_abbrev _ | Psig_modtypesubst _ | Psig_open _
-     |Psig_include _ | Psig_class _ | Psig_class_type _ | Psig_attribute _
-     |Psig_extension _ ->
+     |Psig_modtypesubst _ | Psig_open _ | Psig_include _ | Psig_class _
+     |Psig_class_type _ | Psig_attribute _ | Psig_extension _ ->
         false
 
   let payload = function
