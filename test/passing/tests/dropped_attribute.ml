d32387eb50d2a98bcd38d5cb9ebe2c7d792c2e82
Thomas Del Vecchio <tdelvecchio@janestreet.com>
2024-11-12 15:38:40 -0500

    Fix dropped attribute on package types.
    Signed-off-by: Thomas Del Vecchio <tdelvecchio@janestreet.com>

--- test/passing/tests/dropped_attribute.ml
+++ test/passing/tests/dropped_attribute.ml
@@ -1,8 +1,6 @@
 (* Attributes [[@annot1]] and [[@annot3]] are dropped by the compiler;
    an upcoming patch should fix this. *)
 
-(* CR-soon tdelvecchio: [[@annot4]] is dropped by ocamlformat. *)
-
 module _ : sig
   val foo : (module T [@annot1] with type t = 'a [@annot2]) -> unit
 end = struct
