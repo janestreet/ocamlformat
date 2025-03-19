94a914606dc288824cafbabb627dc69601965137
David Vulakh <dvulakh@janestreet.com>
2025-03-19 12:15:07 -0400

    add test of bad [function]
    Signed-off-by: David Vulakh <dvulakh@janestreet.com>

--- test/passing/tests/source.ml
+++ test/passing/tests/source.ml
@@ -8225,3 +8225,11 @@ end
 let _ = List.sum l (module M) ~f:(fun x -> print_endline "*"; x)
 let _ = List.sum l (module M : S with type t = t) ~f:(fun x -> print_endline "*"; x)
 let _ = List.sum l (module M : S with type t = t and type u = u) ~f:(fun x -> y; x)
+
+let _ =
+  M.ffffffffffffff
+    ~aaaaaaaaaaaaaaaaaaaa:(ffffffffffffff xxxxxxxxxxxxxxxxxxxxxxxxxxx)
+    ~aaaaaaaaaaaaaaaaaaaa:(function
+      | _ -> ffffffff xxxxxxxxxxxxxxxxx
+      | _ -> ffffffff xxxxxxxxxxxxxxxxx)
+
