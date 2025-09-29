97b4841150882799245ad83f59396a4646c20eb1
Ryan Tjoa <rtjoa@janestreet.com>
2025-09-29 14:06:52 -0400

    Respond to review

--- test/passing/tests/type_param_with_jkind_and_attr.ml
+++ test/passing/tests/type_param_with_jkind_and_attr.ml
@@ -9,3 +9,11 @@ type (!'a[@foo] : value, !_[@foo] : value) t
 type _[@foo] t
 
 type 'a[@foo] t
+
+(* test comment preservation *)
+
+type (* 01 *) ( (* 02 *) _ (* 03 *) [@foo] (* 04 *) : (* 05 *) value (* 06 *) ) (* 07 *) t (* 08 *)
+
+type (* 09 *) ( (* 10 *) 'a (* 11 *) [@foo] (* 12 *) : (* 13 *) value (* 14 *) ) (* 15 *) t (* 16 *)
+
+type (* 17 *) ( (* 18 *) 'a (* 19 *) [@foo] (* 20 *) : (* 21 *) value (* 22 *) , (* 23 *) _ (* 24 *) [@foo] (* 25 *) : (* 26 *) value (* 27 *) ) (* 28 *) t (* 29 *)
