94332a54ce417f77240a21f5cb3c0e390a9e9307
Andrej Ivaskovic <aivaskovic@janestreet.com>
2025-11-17 13:42:56 +0000

    Add more comments into quotations tests.

--- test/passing/tests/quotations.ml
+++ test/passing/tests/quotations.ml
@@ -76,6 +76,15 @@ let _ =
        and should be processed correctly *) + 987654321
     - 654987987321654]>
 
+let _ =
+  <[(* a *)
+    1 (* b *) + 2
+    + 3 + (* c *) 4
+    + (* dd *)
+      (* e *) 5 (* f *) + 6
+    (* ggg *)
+    (* h *) + 7 (* ii *)]>
+
 (* Attributes *)
 
 let _ =
