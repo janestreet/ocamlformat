d15696e9df4f4aaa7e344c4d80bbbca0e91c08bd
Jakub Bachurski <kbachurski@gmail.com>
2026-04-16 11:42:25 +0100

    Add tests

--- test/passing/tests/quotations.mli
+++ test/passing/tests/quotations.mli
@@ -5,3 +5,65 @@ type t = <[int]>
 type 'a t = <[$'a list -> $'a option]>
 
 val foo : <[int]> expr
+
+type 'a t = <[$'a]>
+
+type 'a t = $(<['a]>)
+
+type 'a t =
+  <[    ($'a -> $'a -> $'a -> $'a)
+     -> ($'a -> $'a -> $'a -> $'a)
+     -> ($'a -> $'a -> $'a -> $'a)
+     -> ($'a -> $'a -> $'a -> $'a) ]>
+
+(* These cases are probably impossible *)
+type 'a t = $(int * float)
+
+type 'a t = $(#(int * float))
+
+(** Quotes **)
+
+(* Attributes *)
+
+type t = <[(int[@attr])]>
+
+type 'a t = <[('a list[@attr])]>
+
+(* Comments *)
+
+type t = <[int (* post *)]>
+
+type t = <[(* pre *) int]>
+
+type t = <[int (* in *) list]>
+
+(* Attributes & comments *)
+
+type t = <[(int[@attr] (* post *))]>
+
+type t = <[((* pre *) int[@attr])]>
+
+(** Splices **)
+
+(* Attributes *)
+
+type t = <[$(int[@attr])]>
+
+type 'a t = <[$(('a list[@attr]))]>
+
+type 'a t =
+  <[($'a option[@attr]) -> ($'a option[@attr]) -> ($'a list[@attr])]>
+
+(* Comments *)
+
+type t = $int (* post *)
+
+type t = $(* pre *) int
+
+type t = $(int (* in *) list)
+
+(* Attributes & comments *)
+
+type t = $(int[@attr] (* post *))
+
+type t = $((* pre *) int[@attr])
